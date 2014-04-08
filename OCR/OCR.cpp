05ff2726c31a1ed15295b2a4cf5f226560ff71e9
Melvin van den Berg <melvin_skyline@live.nl>
2014-04-08 22:44:43 +0200

    massive license plate recognizer

--- OCR/OCR.cpp
+++ OCR/OCR.cpp
@@ -8,6 +8,9 @@
 #include "splitLicensePlate.h"
 #include "OCRPatternMatching.h"
 
+#include <cstdlib>
+#include "dirent.h"
+
 using namespace ImageLib;
 
 
@@ -18,48 +21,62 @@ int main(int argc, char *argv[])
 	// the example kenteken.png has to be in the folder C:\Images
 	//Data::getInstance().initializeDefines(argv);
 
+	DIR *pDIR;
+	struct dirent *entry;
+	if (pDIR = opendir("C:\\Images\\recognize\\")){
+		while (entry = readdir(pDIR)){
+			if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+				std::string filename = entry->d_name;
+				if (filename.substr(filename.length() - 4, 4) != ".png")
+					continue;
+				std::cout << "Start recognition of " << filename << std::endl;
+				std::unique_ptr<ImageGray> image(loadImg("C:\\Images\\recognize\\" + filename));
+				splitLicensePlate* makeSplit = new splitLicensePlate(*image);
+				std::vector<ImageGray> characters = makeSplit->ProcessImage();
+				//save image 
+				int number = 0;
+				for (ImageGray &character : characters) {
+					corona::Image* destination = corona::CreateImage(character.width(), character.height(), corona::PF_R8G8B8);
+					unsigned char * pixels = (unsigned char*)destination->getPixels();
+					//unsigned char * pixels = new unsigned char[character.width() * character.height() * 3];
+					int count = 0;
+					for (std::vector<unsigned char>::iterator it = character.begin(); it != character.end(); ++it) {
+						pixels[count] = *it;
+						pixels[count + 1] = *it;
+						pixels[count + 2] = *it;
+						count += 3;
+					}
+					std::string path = "C:\\Images\\test";
+					path += std::to_string(number);
+					path += ".png";
+					corona::SaveImage(path.c_str(), corona::FF_PNG, destination);
+					++number;
+				}
+				makeSplit->WriteCSV(0, 0);
 
+				//char recognition starts here
+				OCRPatternMatching matching;
+				number = 0;
+				for (ImageGray &character : characters) {
+					char sdgh = matching.Recognize(character);
 
-	std::unique_ptr<ImageGray> image(loadImg("C:\\Images\\chars.png"));
-	splitLicensePlate* makeSplit = new splitLicensePlate(*image);
-	std::vector<ImageGray> characters = makeSplit->ProcessImage();
-	//save image 
-	int number = 0;
-	for (ImageGray &character : characters) {
-		corona::Image* destination = corona::CreateImage(character.width(), character.height(), corona::PF_R8G8B8);
-		unsigned char * pixels = (unsigned char*) destination->getPixels();
-		//unsigned char * pixels = new unsigned char[character.width() * character.height() * 3];
-		int count = 0;
-		for (std::vector<unsigned char>::iterator it = character.begin(); it != character.end(); ++it) {
-			pixels[count] = *it;
-			pixels[count + 1] = *it;
-			pixels[count + 2] = *it;
-			count += 3;
+					/*
+					splitLicensePlate* makeCsv = new splitLicensePlate(character);
+					charChecker checker = charChecker(makeCsv->csvHorizontal(), character.height(), makeCsv->csvVertical(), true);
+					makeCsv->WriteCSV(1, number);
+					makeCsv->WriteCSV(2, number);
+					char sdgh = checker.process();*/
+
+					std::cout << number << "\t" << sdgh << std::endl;
+					++number;
+				}
+				
+
+			}
 		}
-		std::string path = "C:\\Images\\test";
-		path += std::to_string(number);
-		path += ".png";
-		corona::SaveImage(path.c_str(), corona::FF_PNG, destination);
-		++number;
+		closedir(pDIR);
 	}
-	makeSplit->WriteCSV(0, 0);
 
-	//char recognition starts here
-	OCRPatternMatching matching;
-	number = 0;
-	for (ImageGray &character : characters) {
-		char sdgh = matching.Recognize(character);
-
-		/*
-		splitLicensePlate* makeCsv = new splitLicensePlate(character);
-		charChecker checker = charChecker(makeCsv->csvHorizontal(), character.height(), makeCsv->csvVertical(), true);
-		makeCsv->WriteCSV(1, number);
-		makeCsv->WriteCSV(2, number);
-		char sdgh = checker.process();*/
-
-		std::cout << number << "\t" << sdgh << std::endl;
-		++number;
-	}
 	int bah;
 	std::cin >> bah;
 	return 0;
