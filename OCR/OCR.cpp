421552e7c76417c6e17131fe3a1d41b53dc6782f
melvinvdberg <melvin_skyline@live.nl>
2014-04-09 12:35:49 +0200

    font creation added

--- OCR/OCR.cpp
+++ OCR/OCR.cpp
@@ -17,6 +17,32 @@ using namespace ImageLib;
 
 int main(int argc, char *argv[])
 {
+	/*
+	font creator
+	std::unique_ptr<ImageGray> image(loadImg("C:\\Images\\alle.png"));
+	splitLicensePlate* makeSplit = new splitLicensePlate(*image);
+	std::vector<ImageGray> characters = makeSplit->ProcessImage();
+	//save image 
+	int number = 0;
+	for (ImageGray &character : characters) {
+		corona::Image* destination = corona::CreateImage(character.width(), character.height(), corona::PF_R8G8B8);
+		unsigned char * pixels = (unsigned char*)destination->getPixels();
+		//unsigned char * pixels = new unsigned char[character.width() * character.height() * 3];
+		int count = 0;
+		for (std::vector<unsigned char>::iterator it = character.begin(); it != character.end(); ++it) {
+			pixels[count] = *it;
+			pixels[count + 1] = *it;
+			pixels[count + 2] = *it;
+			count += 3;
+		}
+		std::string path = "C:\\Images\\";
+		path += std::to_string(number);
+		path += ".png";
+		corona::SaveImage(path.c_str(), corona::FF_PNG, destination);
+		++number;
+	}
+	*/
+
 	//Rick, Marco, Michael: Gebruik images.rar in THO7 map. Deze bevat de indeling en samples van C:\images\.
 	int successCount = 0;
 	int failCount = 0;
